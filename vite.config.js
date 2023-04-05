e5677c4e40613964c8143071c58d5805949006d7
PawelNorek <122644701+PawelNorek@users.noreply.github.com>
2023-04-05 23:39:42 +0200

    eslint removed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint' //dodajemy tą linię
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), eslint()], //i tutaj eslint()
+	plugins: [react()],
 })
